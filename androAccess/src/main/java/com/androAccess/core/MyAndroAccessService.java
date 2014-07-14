1d8f6c2a464507ceb86800261916e0633b3c8dda
talhaozcan <a.talhaozcan@gmail.com>
2014-07-14 04:31:41 -0400

    accessibility class is added

--- androAccess/src/main/java/com/androAccess/core/MyAndroAccessService.java
+++ androAccess/src/main/java/com/androAccess/core/MyAndroAccessService.java
@@ -3,7 +3,7 @@ package main.java.com.androAccess.core;
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
-public class MyAndrAccessService extends AccessibilityService{
+public class MyAndroAccessService extends AccessibilityService{
 	
 	@Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
